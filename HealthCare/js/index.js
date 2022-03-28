b04a2054760c2b963cbcb3814ada4d7f0004f5c8
MrBekzod0899 <bekzodqurbonov1399@gmail.com>
2022-03-28 23:53:11 +0500

    modal created

--- HealthCare/js/index.js
+++ HealthCare/js/index.js
@@ -1,1 +1,16 @@
 let menuitems=document.querySelectorAll('.side-menu li')
+let tabledoctor=document.querySelectorAll('.tabledoctor tbody')
+console.log(tabledoctor)
+
+let showModal=document.querySelector('.modalExprience')
+
+function addExprience(){
+    showModal.classList.add('show')
+}
+
+function cancel(){
+    showModal.classList.remove('show')
+}
+function save(){
+    showModal.classList.remove('show')
+}
